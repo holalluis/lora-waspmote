09ad610d8082432aeee847d389c5b0f280f4130b
holalluis <holalluis@gmail.com>
2018-06-28 15:26:54 +0200

    canvis

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Edita aquest arxiu per configurar la connexió serial
+Configura la connexió serial aquí
 
 exemple:
   import config as c
-  ser = serial.Serial()
+  ser=serial.Serial()
   ser.port=c.port
 '''
 
-'''port'''
+'''serial port'''
 #ser.port='COM1'                   #windows
 #ser.port='/dev/ttyUSB0'           #linux
 port='/dev/tty.usbserial-AI03NPY0' #macosx
 
 '''other parameters'''
-baudrate=38400 #9600, 14400, 19200, 28800, 38400, 57600, or 115200
-bytesize=8
-parity='N' #none
-stopbits=1
-timeout=1
-xonxoff=False
-rtscts=False
-dsrdtr=False
+baudrate = 38400 #9600 14400 19200 28800 38400 57600 115200
+bytesize = 8     #8
+parity   = 'N'   #none even
+stopbits = 1     #1
+timeout  = 1     #1
+xonxoff  = False
+rtscts   = False
+dsrdtr   = False
 
